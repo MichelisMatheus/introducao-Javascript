97c5a1ba6192d4b9c4be3adc7e712e4b1249ad2b
Matheus Michelis <basconian.michelis@gmail.com>
2020-10-03 16:11:43 -0300

    Aula 02
    Aula sobre arrays e dicionários

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Aula 01
 //Variáveis
 var nome = "Matheus Michelis";
 var idade = 26;
@@ -28,3 +29,30 @@ console.log(frase.toUpperCase());
 console.log(frase.replace("Japão", "Brasil").toUpperCase());
 console.log(frase.replace("Japão", "Brasil").toLowerCase());
 console.log(n1 * n2);
+
+/* ------------------------------------------------------------------------------------------------------------------------------ */
+
+//Aula 02
+//Variáveis
+var lista = ["Maçã", "Pêra", "Laranja"]; //Cria uma array
+var fruta = {nome: "Maçã", cor: "Vermelha"}; //Cria um dicionário
+var frutas = [{nome: "Maçã", cor: "Vermelha"}, {nome:"Laranja", cor: "Laranja"}, {nome: "Pêra", cor: "Verde"}]; //Cria uma lista de dicionários
+
+//Script
+console.log(lista); //Mostra a array no console
+console.log(lista[1]);
+lista.push("Uva"); //Adicionar um elemento no final da lista
+console.log(lista);
+lista.pop(); //Remover o último elemento da lista
+console.log(lista);
+console.log(lista.length); //Mostra o número de elementos da lista
+console.log(lista.reverse()); //Traz os elementos da lista na ordem inversa 
+console.log(lista.toString()); //Converte a array para uma string
+console.log(lista[0]); //Imprime o primeiro elemento da lista
+console.log(lista.toLocaleString()[0]); //Imprime a primeira letra da string
+console.log(lista.join(" | ")); //Transforma a array em uma string e separa os elementos de acordo com o desejado
+console.log(fruta); //Imprime no console o elemento
+console.log(fruta.nome); //Imprime o atributo "nome" da variável "fruta"
+console.log(frutas[0].nome); //Imprime o elemento "nome" do primeiro elemento da array "frutas"
+
+/* ------------------------------------------------------------------------------------------------------------------------------ */
