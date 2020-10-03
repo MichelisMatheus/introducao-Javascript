6fe2ff0773f78ec54eafeb3db285d9cd348fba88
Matheus Michelis <basconian.michelis@gmail.com>
2020-10-03 15:21:27 -0300

    Aula 01
    Criação do index e do main.js

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//Variáveis
+var nome = "Matheus Michelis";
+var idade = 26;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+var n1 = 5;
+var n2 = 7;
+
+/*
+Comentário de multiplas linhas
+*/
+
+//Scripts - ALerts
+/*
+alert("Meu primeiro JS");
+alert("Bem vindo " + nome);
+alert(nome + " tem " + idade + " anos");
+alert(idade + idade2);
+*/
+
+//Scripts - Console
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toLowerCase());
+console.log(frase.toUpperCase());
+console.log(frase.replace("Japão", "Brasil").toUpperCase());
+console.log(frase.replace("Japão", "Brasil").toLowerCase());
+console.log(n1 * n2);
