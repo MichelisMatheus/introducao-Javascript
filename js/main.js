075d7c4c1a178774c8afdeeb8a18df518ee195ce
Matheus Michelis <basconian.michelis@gmail.com>
2020-10-07 22:52:06 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -133,3 +133,37 @@ console.log(validarIdade(idade));
 */
 
 /* ------------------------------------------------------------------------------------------------------------------------------ */
+
+//Aula 05
+//Variáveis
+
+
+//Scripts
+function clicou() {
+    //alert ("Obrigado por clicar");
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+}
+
+function redirecionar() {
+    //window.open("https://www.google.com.br/"); //abre um link em outra janela
+    window.location.href = "https://www.google.com.br/"; //abre um link na mesma janela
+}
+
+function trocar(elemento) {
+    document.getElementById("passe_o_mouse_aqui").innerHTML = "Obrigado por passar o mouse";
+    //alert("Trocar texto");
+    elemento.innerHTML = "Tira esse mouse de cima de mim!!!!!!!!!";
+}
+
+function voltar(elemento) {
+    document.getElementById("passe_o_mouse_aqui").innerHTML = "Passe o mouse aqui!";
+    elemento.innerHTML = "Não passe o mouse aqui!";
+}
+
+function load() {
+    alert("Página carregada");
+}
+
+function change(elemento) {
+    console.log(elemento.value);
+}
